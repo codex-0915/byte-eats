0bd1484f40dba814363cb32e03c1315289b27d44
Kyl Justin Perez <perezkyljustin@gmail.com>
2025-11-06 16:40:44 +0800

    chore: add custom nativewind config and assets

--- app/index.tsx
+++ app/index.tsx
@@ -2,9 +2,9 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl text-center font-bold text-blue-500">
-        Welcome to Expo Kit Plus
+    <View className="flex-1 items-center justify-center bg-dark-100">
+      <Text className="text-5xl text-center font-bold text-primary font-quicksand">
+        Welcome to ByteEats!
       </Text>
     </View>
   );
--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,11 @@ module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    rules: {
+      // Enforce 2-space indentation
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'react/jsx-indent': ['error', 2],
+      'react/jsx-indent-props': ['error', 2],
+    },
   },
 ]);
