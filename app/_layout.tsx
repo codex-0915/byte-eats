4c6ea00cf383334fd52de6b289623590e5577226
Kyl Justin Perez <perezkyljustin@gmail.com>
2025-11-08 11:19:19 +0800

    chore: apply lint to all project files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,22 @@
-import { SplashScreen, Stack } from "expo-router";
-import { useFonts } from "expo-font";
-import { useEffect } from "react";
+import { SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
 
-import "./globals.css";
+import './globals.css';
 
 export default function RootLayout() {
   const [fontsLoaded, error] = useFonts({
-    "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
-    "QuickSand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
-    "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
-    "QuickSand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
-    "QuickSand-Semibold": require("../assets/fonts/Quicksand-SemiBold.ttf"),  
+    'QuickSand-Bold': require('../assets/fonts/Quicksand-Bold.ttf'),
+    'QuickSand-Light': require('../assets/fonts/Quicksand-Light.ttf'),
+    'QuickSand-Medium': require('../assets/fonts/Quicksand-Medium.ttf'),
+    'QuickSand-Regular': require('../assets/fonts/Quicksand-Regular.ttf'),
+    'QuickSand-Semibold': require('../assets/fonts/Quicksand-SemiBold.ttf'),
   });
 
   useEffect(() => {
     if (error) throw error;
     if (!fontsLoaded) SplashScreen.hideAsync();
   }, [fontsLoaded, error]);
-  
-  return <Stack screenOptions={{ headerShown: false  }} />;
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
