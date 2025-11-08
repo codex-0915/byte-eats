a9526c17cf6044adec08b037da13f4031be1029f
Kyl Justin Perez <perezkyljustin@gmail.com>
2025-11-08 12:19:34 +0800

    feat: add homepage cart button

--- app/index.tsx
+++ app/index.tsx
@@ -3,23 +3,12 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { images, offers } from '@/constants';
 import { Fragment } from 'react';
 import cn from 'clsx';
+import CartButton from '@/components/CartButton';
 
 export default function Index() {
   return (
     <SafeAreaView className={'flex-1 bg-white'}>
-      <View className='flex-between flex-row w-full my-5 px-5'>
-        <View className='flex-start'>
-          <Text className='small-bold text-primary'>DELIVER TO</Text>
-          <TouchableOpacity className='flex-center flex-row gap-x-1 mt-0.5'>
-            <Text className='paragraph-bold text-dark-100'>Current Location</Text>
-            <Image source={images.arrowDown} className={'size-3'} resizeMode={'contain'} />
-          </TouchableOpacity>
-        </View>
-
-        <Text>Cart</Text>
-
-      </View>
-
+      
       <FlatList
         data={offers}
         renderItem={({ item, index }) => {
@@ -54,6 +43,20 @@ export default function Index() {
           );
         }}
         contentContainerClassName='pb-28 px-5'
+        ListHeaderComponent={() => (
+          <View className='flex-between flex-row w-full my-5'>
+            <View className='flex-start'>
+              <Text className='small-bold text-primary'>DELIVER TO</Text>
+              <TouchableOpacity className='flex-center flex-row gap-x-1 mt-0.5'>
+                <Text className='paragraph-bold text-dark-100'>Current Location</Text>
+                <Image source={images.arrowDown} className={'size-3'} resizeMode={'contain'} />
+              </TouchableOpacity>
+            </View>
+
+            <CartButton />
+
+          </View>
+        )} 
       />
       
     </SafeAreaView>
