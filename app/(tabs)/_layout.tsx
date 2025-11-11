432ae4198518080cdb6ae58f27e6d42200c30001
Kyl Justin Perez <perezkyljustin@gmail.com>
2025-11-11 09:23:42 +0800

    feat: add the base routing of files and screens for the app

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Redirect, Slot, Tabs } from 'expo-router'
+
+export default function _Layout() {
+
+  {/* TODO: Remove this after developing auth functionality. 
+            Setting variable to false temporary so I can route to the tab screens */}
+  const isAuthenticated = true;
+
+  if(!isAuthenticated) return <Redirect href="/sign-in" />
+
+  return <Slot />
+}
